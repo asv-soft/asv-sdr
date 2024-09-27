dedb51f2bf98bbc7f7f812a73932709908c4d152
Alexey <248997@gmail.com>
2024-09-27 21:22:10 +0500

    feat: Add GitHub NuGet publishing script and update dependencies
    Removed duplicate NuGet push command from publish_nuget.bat and added new script publish_github.bat for publishing to GitHub Packages. Updated various project file dependencies and removed project references in Asv.Drones.Rsga.

--- publish_github.bat
+++ publish_github.bat
@@ -12,7 +12,7 @@ DEL version.txt
 
 (for %%p in (%projects%) do (
 	cd src\%%p\bin\Release\
-	dotnet nuget push %%p.%VERSION:~1%.nupkg --skip-duplicate --source https://api.nuget.org/v3/index.json
+	dotnet nuget push %%p.%VERSION:~1%.nupkg --skip-duplicate --source https://nuget.pkg.github.com/asv-soft/index.json
 	cd ../../../../
 )) 
 
